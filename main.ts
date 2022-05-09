accb216b1cbf22c3e9fd9a153257315ade5f4ffe
TheYX144 <94316832+TheYX144@users.noreply.github.com>
2022-05-09 18:47:58 -0400

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,36 @@
+namespace SpriteKind {
+    export const PhysicsEnabled = SpriteKind.create()
+    export const hidden = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.PhysicsEnabled, SpriteKind.Player, function (sprite, otherSprite) {
+    moveRandom(PingPong_Ball)
+})
+function moveRandom (mySprite: Sprite) {
+    mySprite.setVelocity(randint(-100, 100), randint(-100, 0))
+}
+let PingPong_Ball: Sprite = null
 let PlayerSpr = sprites.create(assets.image`PaddleTXTR`, SpriteKind.Player)
 controller.moveSprite(PlayerSpr, 100, 0)
 PlayerSpr.setStayInScreen(true)
 PlayerSpr.setPosition(80, 100)
+PingPong_Ball = sprites.create(assets.image`ballTXTR`, SpriteKind.PhysicsEnabled)
+PingPong_Ball.setBounceOnWall(true)
+let pingpongfollow = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.hidden)
+PingPong_Ball.setVelocity(0, 53)
